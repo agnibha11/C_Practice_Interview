ba6a3a879dbf9333240d799cacde31fc777fb7e1
Agnibha Sarkar <agnibhasarkar11@gmail.com>
2026-08-03 20:34:43 +0530

    Question 10 Completed

--- solutions/DSA/q10.cpp
+++ solutions/DSA/q10.cpp
@@ -1,19 +1,19 @@
 #include <cstdio>
 using namespace std;
 
+//count trailing zeros
+int count_zero(int val){
+    //we continuously bitshift until we find the lowest 1
+    int count = 0;
+    while(!(val & 1u)){
+        val >>= 1;
+        count++;
+    }
+    return count;
+}
+
 int main(){
-    int a = 0x00081000;
-
-    //extract the lowest set bit
-    //we can invert the bits a and and then add 1 to it, that will put a 1 in the lowest set bit and the bits below it
-    //will all be 0, then we can and it with itself, now only the bit that will pass is the lowest set bit
-
-    int lowestSet = a & ((~a)+1);
-    
-    //clear the lowest set bit
-    int clearSet = a & (a-1);
-
-    printf("Lowest set bit: %0x\n", lowestSet);
-    printf("Lowest cleared num: %0x\n",clearSet);
+    int a = 0xffff0000;
+    printf("Trailing zeros: %d\n", count_zero(a));
     return 0;
 }
